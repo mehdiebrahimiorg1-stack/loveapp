bc455c6b48a8303be83f64acb72f920aef728b18
Mehdi <mehdi@gmail.com>
2026-07-29 16:00:11 +0330

    fix allowed hosts

--- applications/settings.py
+++ applications/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-i543m@m76b@zld&xlq-v(ya$#00^#o&n8i9t6kzb-(s6kluw(7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition

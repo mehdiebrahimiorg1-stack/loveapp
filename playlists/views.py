8a1c67bc86225953be026f12076c29d5a1ef16f7
Mehdi <mehdi@gmail.com>
2026-07-31 03:45:39 +0330

    edited15_3

--- playlists/views.py
+++ playlists/views.py
@@ -25,28 +25,42 @@ def get_playlist(request, code):
 def add_photo(request, code):
     try:
         playlist = Playlist.objects.get(code=code)
-        # playlist رو از URL میگیریم، از request نمیگیریم
-        data = request.data.copy()
-        data['playlist'] = playlist.id
-        serializer = PhotoSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save(playlist=playlist)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        if 'image' not in request.FILES:
+            return Response({'error': 'عکس نیست'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        photo = Photo()
+        photo.playlist = playlist
+        photo.image = request.FILES['image']
+        photo.caption = request.data.get('caption', '')
+        photo.save()
+        
+        serializer = PhotoSerializer(photo)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
     except Playlist.DoesNotExist:
         return Response({'error': 'کد اشتباهه!'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 @api_view(['POST'])
 def add_song(request, code):
     try:
         playlist = Playlist.objects.get(code=code)
-        data = request.data.copy()
-        data['playlist'] = playlist.id
-        serializer = SongSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save(playlist=playlist)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        if 'file' not in request.FILES:
+            return Response({'error': 'فایل موزیک نیست'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        song = Song()
+        song.playlist = playlist
+        song.title = request.data.get('title', 'موزیک')
+        song.file = request.FILES['file']
+        song.save()
+        
+        serializer = SongSerializer(song)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
     except Playlist.DoesNotExist:
         return Response({'error': 'کد اشتباهه!'}, status=status.HTTP_404_NOT_FOUND)
     except Exception as e:

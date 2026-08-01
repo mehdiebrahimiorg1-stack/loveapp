3d8f73ee5e339cd73f43c790918dde857135bcc3
Mehdi <mehdi@gmail.com>
2026-08-01 15:19:45 +0330

    editedd

--- playlists/urls.py
+++ playlists/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create_playlist, name='create_playlist'),
+    path('gallery-upload/', views.upload_gallery, name='upload_gallery'),
     path('<str:code>/', views.get_playlist, name='get_playlist'),
     path('<str:code>/add-photo/', views.add_photo, name='add_photo'),
     path('<str:code>/add-song/', views.add_song, name='add_song'),
-    path('gallery/upload/', views.upload_gallery, name='upload_gallery'),
 ]

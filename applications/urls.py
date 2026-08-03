e2737984ef68c83818576d97903febb3808dfe45
Mehdi <mehdi@gmail.com>
2026-08-03 20:32:29 +0330

    edited guni

--- applications/urls.py
+++ applications/urls.py
@@ -1,26 +1,14 @@
-"""
-URL configuration for applications project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from playlists import views as gallery_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/playlists/', include('playlists.urls')),
-    path('api/gallery/', include('playlists.urls')),
+    path('api/gallery/check/', gallery_views.check_assets),
+    path('api/gallery/chunked-upload/init/', gallery_views.chunked_upload_init),
+    path('api/gallery/chunked-upload/chunk/', gallery_views.chunked_upload_chunk),
+    path('api/gallery/chunked-upload/complete/', gallery_views.chunked_upload_complete),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
